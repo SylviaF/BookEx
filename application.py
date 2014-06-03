44dc9ca8450332906329397354b3a99707985c57
Jackie Lam <jackielin1992@gmail.com>
2014-06-03 10:50:25 +0800

    API: Books database operation

--- application.py
+++ application.py
@@ -4,12 +4,20 @@ from urls import urls
 
 import tornado.web
 import os
+import pymongo
+
 SETTINGS = dict(
-template_path=os.path.join(os.path.dirname(__file__), "templates"),
-static_path=os.path.join(os.path.dirname(__file__), "static"),
+	template_path=os.path.join(os.path.dirname(__file__), "templates"),
+	static_path=os.path.join(os.path.dirname(__file__), "static"),
+	debug=True,
 )
+
+#database connection
+conn = pymongo.Connection("localhost", 27017)
 
 application = tornado.web.Application(
                 handlers = urls,
                 **SETTINGS
 )
+
+application.db = conn["bookex"]
--- urls.py
+++ urls.py
@@ -1,9 +1,11 @@
 #coding:utf-8
 
 from handlers.index import MainHandler
+from handlers.book import BookHandler, AddBookHandler, DeleteBookHandler
 
 urls = [
     (r'/', MainHandler),
+    (r'/api/v1/books', BookHandler),
+    (r'/api/v1/books/add', AddBookHandler),
+    (r'/api/v1/books/delete', DeleteBookHandler),
 ]
-
-
