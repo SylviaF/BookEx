8a537bfce3f2b778555c3f11f72c0c27f6bd74ec
SylviaF <fangsm@mail2.sysu.edu.cn>
2014-06-07 01:05:34 +0800

    Update application.py

--- application.py
+++ application.py
@@ -1,23 +1,31 @@
 #coding:utf-8
 
-from urls import urls
+from handlers.index import *
+from handlers.user import LoginHandler
+from handlers.user import LogoutHandler
+from handlers.user import RegisterHandler
+from handlers.user import UserInfoHandler
+from handlers.index import MainHandler
+from handlers.book import BookHandler, AddBookHandler, DeleteBookHandler
+from handlers.save import SaveBookHandler
+from handlers.borrow import BorrowBookHandler
+from handlers.comment import CommentHandler
 
-import tornado.web
-import os
-import pymongo
-
-SETTINGS = dict(
-	template_path=os.path.join(os.path.dirname(__file__), "templates"),
-	static_path=os.path.join(os.path.dirname(__file__), "static"),
-	debug=True,
-)
-
-#database connection
-conn = pymongo.Connection("localhost", 27017)
-
-application = tornado.web.Application(
-                handlers = urls,
-                **SETTINGS
-)
-
-application.db = conn["bookex"]
+urls = [
+    (r'/', MainHandler),
+	(r'/found', FoundHandler),
+	(r'/notfound', NotFoundHandler),
+	(r'/addbook', AddBookShowHandler),
+	(r'/homepage', HomepageHandler),
+	(r'/book', BookShowHandler),
+    (r'/api/v1/users/login', LoginHandler),
+    (r'/api/v1/users/logout', LogoutHandler),
+    (r'/api/v1/users/register', RegisterHandler),
+	(r'/api/v1/info', UserInfoHandler),
+    (r'/api/v1/books', BookHandler),
+    (r'/api/v1/books/add', AddBookHandler),
+    (r'/api/v1/books/delete', DeleteBookHandler),
+    (r'/api/v1/savebook', SaveBookHandler),
+    (r'/api/v1/borrowbook', BorrowBookHandler),
+    (r'/api/v1/comment', CommentHandler),
+]
